2b4f6088028d6e4a7b25860e83e5760ac2106d2b
ravi-ydv <56464616+ravi-ydv@users.noreply.github.com>
2026-05-07 11:37:14 +0530

    feat: Implement resource for credentials (#14)
    * feat: add resources for cloud connector, container registry, kubernetes config, and webhook secret credentials
    
    * feat: add identity block for all resources
    
    * test: rerecord test fixture
    
    * tests: remove count check for resources and datasources
    
    ---------
    
    Co-authored-by: ravi-ydv <raviyadavrr37@gmail.com>

---
+++ examples/resources/btpservice_cicd_credential_container_registry/import.sh
@@ -0,0 +1,18 @@
+terraform import btpservice_cicd_credential_container_registry.<resource_name> <id>
+terraform import btpservice_cicd_credential_container_registry.example dd005d8b-1fee-4e6b-b6ff-cb9a197b7fe0
+
+
+# terraform import using id attribute in import block
+
+import {
+  to = btpservice_cicd_credential_container_registry.<resource_name>
+  id = "<id>"
+}
+
+import {
+  to =  btpservice_cicd_credential_container_registry.<resource_name>
+  identity = {
+   id = "<id>"
+  }
+}
+
