73f524f2b8a48b4d021e584990a5132254abde7e
ravi-ydv <ravi.yadav03@sap.com>
2026-06-16 08:04:18 +0000

    refactor: move types to new file

--- btpservices/provider/cicd/settings/resource_allowed_spaces.go
+++ btpservices/provider/cicd/settings/resource_allowed_spaces.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	cicdclient "github.com/SAP/terraform-provider-btp-services/internal/cicd/client"
 	cicdmodels "github.com/SAP/terraform-provider-btp-services/internal/cicd/models"
@@ -141,38 +140,4 @@ func (r *allowedSpacesResource) Delete(ctx context.Context, _ resource.DeleteReq
 	if err := r.cli.AllowedSpaces.Set(ctx, cicdmodels.AllowedSpaceListDTO{AllowedSpaces: []cicdmodels.AllowedSpace{}}); err != nil {
 		resp.Diagnostics.AddError("Error Clearing Allowed Spaces", err.Error())
 	}
-}
-
-// allowedSpacesModel is the Terraform state model.
-type allowedSpacesModel struct {
-	AllowedSpaces []allowedSpaceEntry `tfsdk:"allowed_spaces"`
-}
-
-type allowedSpaceEntry struct {
-	SpaceGUID types.String `tfsdk:"space_guid"`
-	Comment   types.String `tfsdk:"comment"`
-}
-
-func allowedSpacesValueFrom(v cicdmodels.AllowedSpacesResponse) allowedSpacesModel {
-	entries := make([]allowedSpaceEntry, 0, len(v.AllowedSpaces))
-	for _, s := range v.AllowedSpaces {
-		entries = append(entries, allowedSpaceEntry{
-			SpaceGUID: types.StringValue(s.SpaceGUID),
-			Comment:   types.StringValue(s.Comment),
-		})
-	}
-	return allowedSpacesModel{
-		AllowedSpaces: entries,
-	}
-}
-
-func (m allowedSpacesModel) toRequest() cicdmodels.AllowedSpaceListDTO {
-	spaces := make([]cicdmodels.AllowedSpace, 0, len(m.AllowedSpaces))
-	for _, e := range m.AllowedSpaces {
-		spaces = append(spaces, cicdmodels.AllowedSpace{
-			SpaceGUID: e.SpaceGUID.ValueString(),
-			Comment:   e.Comment.ValueString(),
-		})
-	}
-	return cicdmodels.AllowedSpaceListDTO{AllowedSpaces: spaces}
 }
